67f4a28c064ccff0a6decb076467c771a688f21d
davidlinke <davidlinke@davidlinke.com>
2019-07-16 12:02:59 -0400

    experimenting with accessing data, currently takes way too long to load

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-console.log(agency);
+// console.log(stops);
+
+// const printStops = (stopObject, index) => {
+// 	console.log(stopObject.stop_name);
+// };
+
+// stops.forEach(printStops);
+
+//HARDCODE STOPS FOR TESTING
+const westport = 134; // Westport stop_id
+const grandcentral = 1; // Grand Central stop_id
+
+let westportStops = stop_times.filter(obj => {
+	return obj.stop_id === westport;
+});
+
+let grandCentralStops = stop_times.filter(obj => {
+	return obj.stop_id === grandcentral;
+});
+
+const tripsBetweenGrandCentralAndWestport = () => {
+	const trips = [];
+	// for (let i = 0; i < westportStops.length; i++) {
+	// 	let foundInGrandCentral = false;
+	// 	while (!foundInGrandCentral) {
+	// 		let j = 0;
+	// 		console.log(`Grand Central Trip ID: ${grandCentralStops[j].trip_id}`);
+	// 		console.log(`Westport Trip ID: ${westportStops[j].trip_id}`);
+	// 		if (grandCentralStops[j].trip_id === westportStops[i].trip_id) {
+	// 			trips.push(grandCentralStops[j].trip_id);
+	// 			foundInGrandCentral = true;
+	// 		}
+	// 		j++;
+	// 	}
+	// }
+	console.log(trips);
+};
+
+// tripsBetweenGrandCentralAndWestport();
+
+// console.log(westportStops);
