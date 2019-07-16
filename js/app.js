cad3c8ba5ee9d2cdb5281e3bdbc02876b62ff24c
davidlinke <davidlinke@davidlinke.com>
2019-07-16 14:05:11 -0400

    added get zipcode lat/lng from mapquest API

--- js/app.js
+++ js/app.js
@@ -1,23 +1,56 @@
-const apikey = '2180450fb6c595540f84351dfaa20450';
-const lat = '41.042950';
-const long = '-73.532360';
-const time = Math.round(new Date().getTime() / 1000); // get unix time
-const tomorrowtime = time + 86400;
-console.log(time);
-console.log(tomorrowtime);
-const endpoint = `https://api.darksky.net/forecast/${apikey}/${lat},${long},${time}?exclude=currently,flags,minutely,daily,alerts`;
-const endpointTomorrow = `https://api.darksky.net/forecast/${apikey}/${lat},${long},${tomorrowtime}?exclude=currently,flags,minutely,daily,alerts`;
+const darkSkyAPIKey = '2180450fb6c595540f84351dfaa20450';
+const mapQuestAPIKey = 'db5wGqWH4xQFSlu6TGvorQMoRG8daxn5';
 
-$.ajax({
-	url: endpoint,
-	dataType: 'jsonp' // NEED TO SPECIFY THIS FOR IT TO WORK
-}).then(data => {
-	console.log(data);
+// MORNING DEFINED AS 7AM TO 12PM
+// AFTERNOON DEFINED AS 12PM TO 5PM
+// EVENING DEFINED AS 5PM TO 10PM
+// OVERNIGHT DEFINED AS 10PM TO 7AM
+
+$('#zipCodeSubmit').on('click', () => {
+	let zipcode = $('#zipCodeInput').val();
+	// console.log(zipcode);
+	getlatLong(zipcode);
 });
 
-$.ajax({
-	url: endpointTomorrow,
-	dataType: 'jsonp' // NEED TO SPECIFY THIS FOR IT TO WORK
-}).then(data => {
-	console.log(data);
-});
+const getlatLong = zipcode => {
+	const mapQuestEndpoint = `http://open.mapquestapi.com/geocoding/v1/address?key=${mapQuestAPIKey}&location=${zipcode}`;
+
+	$.ajax({
+		url: mapQuestEndpoint
+	}).then(data => {
+		console.log(data.results[0].locations[0].latLng);
+		const latLngObject = data.results[0].locations[0].latLng;
+		getWeatherData(latLngObject);
+	});
+};
+
+const getWeatherData = latLngObject => {
+	const lat = latLngObject.lat;
+	const lng = latLngObject.lng;
+
+	// console.log(`lat is ${lat} and type ${typeof lat}`);
+	// console.log(`lng is ${lng} and type ${typeof lng}`);
+
+	const currentTime = Math.round(new Date().getTime() / 1000); // get unix time
+	const tomorrowsTime = currentTime + 86400;
+
+	const darkSkyEndpoint = `https://api.darksky.net/forecast/${darkSkyAPIKey}/${lat},${lng},${currentTime}?exclude=currently,flags,minutely,daily,alerts`;
+
+	const darkSkyEndpointTomorrow = `https://api.darksky.net/forecast/${darkSkyAPIKey}/${lat},${lng},${tomorrowsTime}?exclude=currently,flags,minutely,daily,alerts`;
+
+	// GET WEATHER DATA FOR TODAY
+	$.ajax({
+		url: darkSkyEndpoint,
+		dataType: 'jsonp' // NEED TO SPECIFY THIS FOR IT TO WORK
+	}).then(data => {
+		console.log(data);
+	});
+
+	// GET WEATHER DATA FOR TOMORROW
+	$.ajax({
+		url: darkSkyEndpointTomorrow,
+		dataType: 'jsonp'
+	}).then(data => {
+		console.log(data);
+	});
+};
