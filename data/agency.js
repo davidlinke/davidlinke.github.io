975461cdcc9ade3d1323232259e33732f29ad3e9
davidlinke <davidlinke@davidlinke.com>
2019-07-16 10:29:16 -0400

    initial file creation + added data

---
+++ data/agency.js
@@ -0,0 +1,34 @@
+const agency = [
+	{
+		agency_id: 1,
+		agency_name: 'Metro-North Railroad',
+		agency_url: 'http://www.mta.info/mnr',
+		agency_timezone: 'America/New_York',
+		agency_phone: '212-532-4900',
+		agency_lang: 'EN'
+	},
+	{
+		agency_id: 2,
+		agency_name: 'NY Waterway',
+		agency_url: 'http://www.nywaterway.com/',
+		agency_timezone: 'America/New_York',
+		agency_phone: '800-533-3779',
+		agency_lang: 'EN'
+	},
+	{
+		agency_id: 5,
+		agency_name: 'Shore Line East',
+		agency_url: 'http://www.shorelineeast.com',
+		agency_timezone: 'America/New_York',
+		agency_phone: '877-287-4337',
+		agency_lang: 'EN'
+	},
+	{
+		agency_id: 3,
+		agency_name: 'MNR Hudson Rail Link',
+		agency_url: 'http://www.mta.info/mnr/html/raillink/raillinkpage.htm',
+		agency_timezone: 'America/New_York',
+		agency_phone: '212-532-4900',
+		agency_lang: 'EN'
+	}
+];
---
+++ data/calendar_dates.js
@@ -0,0 +1,407 @@
+const calendar_dates = [
+	{
+		service_id: 7668,
+		date: '20190710',
+		exception_type: 1
+	},
+	{
+		service_id: 7667,
+		date: '20190711',
+		exception_type: 1
+	},
+	{
+		service_id: 7674,
+		date: '20190712',
+		exception_type: 1
+	},
+	{
+		service_id: 7675,
+		date: '20190713',
+		exception_type: 1
+	},
+	{
+		service_id: 7676,
+		date: '20190714',
+		exception_type: 1
+	},
+	{
+		service_id: 7683,
+		date: '20190715',
+		exception_type: 1
+	},
+	{
+		service_id: 7693,
+		date: '20190716',
+		exception_type: 1
+	},
+	{
+		service_id: 7693,
+		date: '20190717',
+		exception_type: 1
+	},
+	{
+		service_id: 7693,
+		date: '20190718',
+		exception_type: 1
+	},
+	{
+		service_id: 7698,
+		date: '20190719',
+		exception_type: 1
+	},
+	{
+		service_id: 7699,
+		date: '20190720',
+		exception_type: 1
+	},
+	{
+		service_id: 7700,
+		date: '20190721',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190722',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190723',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190724',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190725',
+		exception_type: 1
+	},
+	{
+		service_id: 7703,
+		date: '20190726',
+		exception_type: 1
+	},
+	{
+		service_id: 7701,
+		date: '20190727',
+		exception_type: 1
+	},
+	{
+		service_id: 7702,
+		date: '20190728',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190729',
+		exception_type: 1
+	},
+	{
+		service_id: 7696,
+		date: '20190730',
+		exception_type: 1
+	},
+	{
+		service_id: 7697,
+		date: '20190731',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190801',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190802',
+		exception_type: 1
+	},
+	{
+		service_id: 7671,
+		date: '20190803',
+		exception_type: 1
+	},
+	{
+		service_id: 7672,
+		date: '20190804',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190805',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190806',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190807',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190808',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190809',
+		exception_type: 1
+	},
+	{
+		service_id: 7688,
+		date: '20190810',
+		exception_type: 1
+	},
+	{
+		service_id: 7689,
+		date: '20190811',
+		exception_type: 1
+	},
+	{
+		service_id: 7683,
+		date: '20190812',
+		exception_type: 1
+	},
+	{
+		service_id: 7693,
+		date: '20190813',
+		exception_type: 1
+	},
+	{
+		service_id: 7697,
+		date: '20190814',
+		exception_type: 1
+	},
+	{
+		service_id: 7696,
+		date: '20190815',
+		exception_type: 1
+	},
+	{
+		service_id: 7694,
+		date: '20190816',
+		exception_type: 1
+	},
+	{
+		service_id: 7675,
+		date: '20190817',
+		exception_type: 1
+	},
+	{
+		service_id: 7676,
+		date: '20190818',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190819',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190820',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190821',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190822',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190823',
+		exception_type: 1
+	},
+	{
+		service_id: 7688,
+		date: '20190824',
+		exception_type: 1
+	},
+	{
+		service_id: 7689,
+		date: '20190825',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190826',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190827',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190828',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190829',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190830',
+		exception_type: 1
+	},
+	{
+		service_id: 7675,
+		date: '20190831',
+		exception_type: 1
+	},
+	{
+		service_id: 7676,
+		date: '20190901',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190902',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190903',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190904',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190905',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190906',
+		exception_type: 1
+	},
+	{
+		service_id: 7688,
+		date: '20190907',
+		exception_type: 1
+	},
+	{
+		service_id: 7689,
+		date: '20190908',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190909',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190910',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190911',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190912',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190913',
+		exception_type: 1
+	},
+	{
+		service_id: 7688,
+		date: '20190914',
+		exception_type: 1
+	},
+	{
+		service_id: 7689,
+		date: '20190915',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190916',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190917',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190918',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190919',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190920',
+		exception_type: 1
+	},
+	{
+		service_id: 7675,
+		date: '20190921',
+		exception_type: 1
+	},
+	{
+		service_id: 7676,
+		date: '20190922',
+		exception_type: 1
+	},
+	{
+		service_id: 7690,
+		date: '20190923',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190924',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190925',
+		exception_type: 1
+	},
+	{
+		service_id: 7691,
+		date: '20190926',
+		exception_type: 1
+	},
+	{
+		service_id: 7692,
+		date: '20190927',
+		exception_type: 1
+	},
+	{
+		service_id: 7688,
+		date: '20190928',
+		exception_type: 1
+	}
+];
