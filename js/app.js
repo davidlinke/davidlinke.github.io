87db56890ebc9be58d50899d658ef170bda834a6
davidlinke <davidlinke@davidlinke.com>
2019-07-18 18:13:31 -0400

    implemented radar modal but still need to generate it dynamically and use the correct location rather than hardcoded

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@
 
 pageLoad('Grabbing your location...'); // Show loading state
 getIP(); // Get IP address, start chain of functions
+
+const $radarButton = $('#viewRadar');
+const $radarModal = $('#radarModalOuter');
+const $radarCloseButton = $('#closeRadar');
+
+$radarButton.on('click', () => {
+	console.log('radar button clicked');
+	$radarModal.css('display', 'block');
+});
+
+// $radarCloseButton.on('click', () => {
+// 	console.log('close radar button clicked');
+// 	$radarModal.css('display', 'none');
+// });
+
+$('body').on('click', event => {
+	console.log(event);
+	if (event.target.id != 'radarModalInner' && event.target.id != 'viewRadar') {
+		console.log('clicked outside of radar');
+		$radarModal.css('display', 'none');
+	}
+});
