fda04ee50c3c8986bbd2b4d126fd2659768c7f33
davidlinke <davidlinke@davidlinke.com>
2019-07-19 12:12:39 -0400

    finished implementing store location in localStorage

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@
 // app.js
 //=================================================
 
-// pageLoad('Grabbing your location...'); // Show loading state
-// getIP(); // Get IP address, start chain of functions
-
 //=================================================
 // checkForPreviousVisit()
 // Checks to see if user has previously visited the site to use the stored location
 //=================================================
 const checkForPreviousVisit = () => {
-	if (localStorage.getItem('location')) {
+	const zip = localStorage.getItem('location');
+	if (zip) {
 		displayInput();
+		updateZipCode(zip);
+		triggerWeatherLookup();
 	} else {
 		pageLoad('Grabbing your location...'); // Show loading state
 		getIP(); // Get IP address, start chain of functions
